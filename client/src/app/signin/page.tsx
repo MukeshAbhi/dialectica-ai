68cf882c527657359d51bb488853e963ced8c7b3
MukeshAbhi <abhimukesh284@gmail.com>
2025-10-17 17:56:34 +0530

    working

--- client/src/app/signin/page.tsx
+++ client/src/app/signin/page.tsx
@@ -1,7 +1,5 @@
 import CustomSignIn from "@/components/auth/custom-sign-in";
 
-export const dynamic = "force-dynamic";
-
 export default function SignInPage() {
   return <CustomSignIn />;
 }
---
+++ client/src/lib/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Twitter from "next-auth/providers/twitter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+   Google,
+   Twitter,
+  ],
+});
