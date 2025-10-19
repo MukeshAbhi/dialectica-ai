61d9843d387af2d0640753c80ffdccb1816c0ac7
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-10-19 19:27:24 +0530

    Sidebar imported in layout for reusability

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
+import React from "react";
+import SideBar from "@/components/navigation/SideBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +35,8 @@ export default function RootLayout({
         <head>
           <link rel="icon" href="/logo.ico" />
         </head>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-screen mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen`}>
+          <SideBar/>
           {children}
         </body>
       </html>
