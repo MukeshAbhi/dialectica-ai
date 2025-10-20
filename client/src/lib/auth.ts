63e7307fc4fe0db6f5a8e2e520779fa71725260d
MukeshAbhi <abhimukesh284@gmail.com>
2025-10-20 20:10:26 +0530

    containerized with docker

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -6,6 +6,7 @@ import prisma from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
