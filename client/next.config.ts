0b821ccf8bf0af0253586bd613ad0dda96d1f120
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2025-10-21 23:50:26 +0530

    added sidebar responsiveness and fixed more routes and other issues

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,6 +6,21 @@ const nextConfig: NextConfig = {
   trailingSlash: false,
   serverExternalPackages: [],
   outputFileTracingRoot: path.join(__dirname, "../"),
+  //image profiles domains for whitelisting
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
